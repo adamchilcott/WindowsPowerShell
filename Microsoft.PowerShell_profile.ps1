aabbe4398faefb87f60156d7a533ab32562c4927
Adam Chilcott <adam.chilcott@github.com>
2020-10-31 13:05:27 +1100

    Fix

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@
 #######################
 
 ## Paths
-Set-Item -Path env:Path -Value ($env:PATH += "$env:USERPROFILE\Documents\WindowsPowerShell\Scripts\Internal")
+Set-Item -Path env:Path -Value ($env:PATH += ";$env:USERPROFILE\Documents\WindowsPowerShell\Scripts\Internal")
 Set-Item -Path env:Path -Value ($env:PATH += ";$env:USERPROFILE\Documents\WindowsPowerShell\Scripts\External")
 
 ## Aliases
